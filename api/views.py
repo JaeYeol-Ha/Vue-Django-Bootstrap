80fbef95ef043d38270ce5f79f0ff289c33becd6
JaeYeol-Ha <gkwoduf08@naver.com>
2021-10-07 18:02:27 +0900

    21.10.07 Vue-Django API 개발 (I) - Serialization 0:30 까지 완료

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse
+from django.views.generic.detail import BaseDetailView
 from django.views.generic.list import BaseListView
 
 from api.utils import obj_to_post
@@ -12,3 +13,12 @@ class ApiPostLV(BaseListView):
         qs = context['object_list']
         postList = [obj_to_post(obj) for obj in qs]
         return JsonResponse(data=postList, safe=False, status=200)
+
+
+class ApiPostDV(BaseDetailView):
+    model = Post
+
+    def render_to_response(self, context, **response_kwargs):
+        obj = context['object']
+        post = obj_to_post(obj)
+        return JsonResponse(data=post, safe=True, status=200)
